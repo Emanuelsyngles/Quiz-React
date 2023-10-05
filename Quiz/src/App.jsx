75374cfd735f215c3a7a9d6faf82ee4aab55d817
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-05 16:03:27 -0300

    ...

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,9 +1,14 @@
+//react, componentes, estáticos
+
 import './App.css'
+
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <>
-      <h1>Quiz App</h1>
+      <h1>Quiz de programação</h1>
+      <Welcome/>
     </>
   )
 }
