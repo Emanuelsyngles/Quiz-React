1377beafdd8e72f8d14956f5afb7850b3b03c951
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-07 17:00:01 -0300

    Adicionando o game over.

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -3,8 +3,8 @@ import { useContext, useEffect } from "react";
 import Welcome from './components/Welcome'
 
 import Question from "./components/Question";
-
 import { QuizContext } from "./context/quiz";
+import GameOver from "./components/GameOver";
 
 import "./App.css";
 
@@ -20,6 +20,7 @@ function App() {
       <h1>Quiz de programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   );
 }
--- quiz/src/components/GameOver.jsx
+++ quiz/src/components/GameOver.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const GameOver = () => {
   return (
     <div>
-      
+      <h2>Game Over</h2>
     </div>
-  )
+  );
 }
 
 export default GameOver
